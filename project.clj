be3b361c68023acbd84e225d6bda48501c75fb1b
Jarppe <jarppe@metosin.fi>
2017-03-07 12:01:30 +0200

    Updated versions

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/clojurescript "1.9.456"]
 
                  ;; Tooling:
 
@@ -15,54 +15,58 @@
 
                  ;; Common:
 
-                 [org.clojure/core.async "0.2.391"]
+                 [org.clojure/core.async "0.3.441"]
                  [prismatic/schema "1.1.3"]
                  [prismatic/plumbing "0.5.3"]
-                 [metosin/potpuri "0.3.0"]
+                 [metosin/potpuri "0.4.0"]
 
                  ;; Backend:
 
-                 [mount "0.1.10"]
+                 [mount "0.1.11"]
                  [metosin/schema-tools "0.10.0-20160425.120043-1"]
-                 [metosin/compojure-api "1.1.8"]
-                 [metosin/ring-swagger-ui "2.2.2-0"]
                  [metosin/lokit "0.1.0"]
-                 [clj-http "3.2.0"]
-                 [hiccup "1.0.5"]
-                 [enlive "1.1.6"]
+                 [clj-http "3.4.1"]
 
                  ; HTTP:
 
                  [http-kit "2.2.0"]
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-devel "1.5.0"]
+                 [ring/ring-core "1.5.1"]
+                 [ring/ring-devel "1.5.1"]
+                 [ring/ring-defaults "0.2.3"]
                  [ring-webjars "0.1.1"]
-                 [ring-middleware-format "0.7.0"]
-                 [metosin/ring-http-response "0.8.0"]
-                 [com.cognitect/transit-clj "0.8.288"]
+                 [ring-middleware-format "0.7.2"]
+                 [metosin/ring-http-response "0.8.2"]
+                 [metosin/compojure-api "1.1.10"]
+                 [metosin/ring-swagger-ui "2.2.8"]
+                 [hiccup "1.0.5"]
+                 [enlive "1.1.6"]
 
                  ; DB:
 
                  [org.clojure/java.jdbc "0.6.2-alpha3"]
-                 [org.postgresql/postgresql "9.4.1210.jre7"]
-                 [hikari-cp "1.7.3"]
+                 [org.postgresql/postgresql "42.0.0"]
+                 [hikari-cp "1.7.5"]
                  [com.layerware/hugsql "0.4.7"]
-                 [org.flywaydb/flyway-core "4.0.3"]
-                 [com.h2database/h2 "1.4.192" :scope "test"]
+                 [org.flywaydb/flyway-core "4.1.2"]
+                 [com.h2database/h2 "1.4.193" :scope "test"]
 
                  ;; Frontend:
 
-                 [reagent "0.6.0-rc"]
-                 [cljs-http "0.1.41"]
+                 [reagent "0.6.0"]
+                 [cljs-http "0.1.42"]
                  [ring-webjars "0.1.1"]
-                 [org.webjars/bootstrap "4.0.0-alpha.3"]
-                 [org.webjars/font-awesome "4.6.3"]
+                 [org.webjars/bootstrap "4.0.0-alpha.6-1"]
+                 [org.webjars/font-awesome "4.7.0"]
                  [org.webjars/es5-shim "4.5.9"]
-                 [binaryage/devtools "0.8.1"]
+                 [binaryage/devtools "0.9.2"]
                  [metosin/reagent-dev-tools "0.1.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-ring "0.9.3"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel "0.5.9" :exclusions [org.clojure/clojure]]
+            [lein-ring "0.11.0"]
+            [lein-deps-tree "0.1.2"]
+            [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
+            [com.jakemccrary/lein-test-refresh "0.18.1"]]
 
   :source-paths ["src/clj"]
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
